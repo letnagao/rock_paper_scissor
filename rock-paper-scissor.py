6dc973ba709895354f791a7109fe6222176e5d4e
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-23 19:19:59 -0300

    implementing random

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,14 +1,15 @@
+import random
 
-computer_choice = 'scissors'
-
+computer_choice = random.choice(['scissors', 'rock', 'paper'])
 user_choice = input('Do you want - rock, paper, or scissors?\n')
 
 if computer_choice == user_choice:
     print('TIE')
 elif user_choice == 'rock' and computer_choice == 'scissors':
-    print('WIN')
+    print('WIN, the computer had ' + computer_choice)
 elif user_choice == 'paper' and computer_choice == 'rock':
-    print('WIN')
+    print('WIN, the computer had ' + computer_choice)
 elif user_choice == 'scissors' and computer_choice == 'paper':
-    print('WIN')
-print('You lose :C Computer wins :D')
+    print('WIN, the computer had ' + computer_choice)
+else:
+    print('You lose :C Computer wins :D')
